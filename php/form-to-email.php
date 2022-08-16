4998d5a2501c0e2e58f19431994735e4820625e6
ndamps <noeldamps@icloud.com>
2022-08-16 21:02:47 +0200

    Some Changes

--- php/form-to-email.php
+++ php/form-to-email.php
@@ -1,15 +1,23 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
-$to = "noeldamps@icloud.com";
 
-$email_from = "noeldamps@icloud.com";
-$email_subject = "Neue Nachricht von $name";
-$email_body = "Nachricht: $message";
-$header = "Neue Nachricht von $name";
+error_reporting(E_ALL);
+ini_set('display_errors','On');
 
-// Send Email
-mail($to,$email_subject,$email_body,$header)
+require_once __DIR__.'/vendor/autoload.php';
+
+$transport = new Swift_SmtpTransport('smtp.gmail.com',465,'ssl');
+$transport->setUsername("noeldamps1@gmail.com");
+$transport->setPassword("oqcpsqlbeypgfhwf")
+
+$mailer = new Swift_Mailer($transport);
+
+$message = new Swift_Message('Test');
+$message->setFrom(['noeldamps@icloud.com']);
+$message->setTo(['noeldamps@icloud.com']);
+$mes = $_POST['message'];
+$message->setBody($mes);
+
+$result = $mailer->send($message);
+
 
 ?>
